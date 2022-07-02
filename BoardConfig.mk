8391d577ff28b1857b085eecd7932690f5200299
GhostMaster69-dev <rathore6375@gmail.com>
2022-07-02 22:10:55 +0530

    vince: make LLVM=1
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -60,7 +60,7 @@ TARGET_KERNEL_VERSION := 4.9
 TARGET_KERNEL_CONFIG := vince-perf_defconfig
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-cosmic
-TARGET_KERNEL_ADDITIONAL_FLAGS := LLVM=1 HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
+TARGET_KERNEL_ADDITIONAL_FLAGS := LLVM=1
 
 # ANT
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
